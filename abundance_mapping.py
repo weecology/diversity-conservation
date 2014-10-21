8988b3c30a49af71eea498ee169065df6acc99f1
Kari Norman <kari@normanfamily.org>
2014-10-21 13:46:23 -0600

    Start mapping sites

--- abundance_mapping.py
+++ abundance_mapping.py
@@ -6,7 +6,6 @@ from numpy import log10
 import pandas as pd
 import numpy
 from mpl_toolkits.basemap import Basemap
-import os
 import macroecotools
 
 data = pd.read_csv('bbs_abundances_by_site.csv', delimiter=',')
@@ -19,4 +18,11 @@ for site, site_data in data_site:
     sites.append(site)
 print len(sites)
 
-macroecotools.richness_in_group(data, ['site'], ['species'])
+richness_by_site = macroecotools.richness_in_group(data, ['site'], ['species'])
+
+map = Basemap(projection='merc',llcrnrlat=-57,urcrnrlat=71, llcrnrlon=-180,urcrnrlon=180,lat_ts=20,resolution='l')
+map.drawcoastlines(linewidth = 1.25)
+lats = data["lat"]
+longs = data["long"]
+x,y = map(longs,lats)
+map.plot(x,y, ls='', marker=markers[i], markerfacecolor=colors[i], markeredgewidth=0.25, markersize=markersizes)
