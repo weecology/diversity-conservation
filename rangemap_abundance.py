f51044a026b756bb68260595e4382aec3b84daf6
Kari Norman <kari@normanfamily.org>
2015-04-06 14:01:02 -0600

    Import sites coordinates using vrt file.

--- rangemap_abundance.py
+++ rangemap_abundance.py
@@ -40,3 +40,17 @@ sites = driver1.Open('bbs_site_coordinates.csv')
 # unique list of site coordinates
 # loop over polygons for each point 
 # list of species names for each site
+from osgeo import ogr
+ogr.UseExceptions()
+
+inDataSource = ogr.Open("example_wrapper.vrt")
+lyr = inDataSource.GetLayer('example')
+for feat in lyr:
+    geom = feat.GetGeometryRef()
+    print geom.ExportToWkt() 
+
+inDataSource = ogr.Open("bbs_sites_coordinate_wrapper.vrt")
+lyr = inDataSource.GetLayer('bbs_sites')
+for feat in lyr:
+    geom = feat.GetGeometryRef()
+    print geom.ExportToWkt() 
