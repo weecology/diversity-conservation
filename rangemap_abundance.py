b503e033750420d29b68f851543bb36f36343489
Kari Norman <kari@normanfamily.org>
2015-04-13 12:34:25 -0600

    Create dataframe of species counts at sites.

--- rangemap_abundance.py
+++ rangemap_abundance.py
@@ -20,33 +20,6 @@ first_feature.items()
 # We can get a particular value by
 first_feature.GetField('SCINAME')
 
-#example intersection    
-wkt1 = "POLYGON ((1208064.271243039 624154.6783778917, 1208064.271243039 601260.9785661874, 1231345.9998651114 601260.9785661874, 1231345.9998651114 624154.6783778917, 1208064.271243039 624154.6783778917))"
-wkt2 = "POLYGON ((1199915.6662253144 633079.3410163528, 1199915.6662253144 614453.958118695, 1219317.1067437078 614453.958118695, 1219317.1067437078 633079.3410163528, 1199915.6662253144 633079.3410163528)))"
-
-poly1 = ogr.CreateGeometryFromWkt(wkt1)
-poly2 = ogr.CreateGeometryFromWkt(wkt2)
-
-intersection = poly1.Intersection(poly2)
-
-print intersection.ExportToWkt()    
-
-#intersection of two species
-first_species = all_sp_layer.GetFeature(1)
-second_species = all_sp_layer.GetFeature(4)
-
-first_species_geom = first_species.GetGeometryRef()
-second_species_geom = second_species.GetGeometryRef()
-
-inter = first_species_geom.Intersection(second_species_geom)
-print inter
-
-for i in range(all_sp_layer.GetFeatureCount()):
-    second_species = all_sp_layer.GetFeature(i+1)
-    second_species_geom = second_species.GetGeometryRef()
-    inter = first_species_geom.Intersection(second_species_geom)
-    print inter
-    
 # Filter features to only include those where Seasonal == 1 or 2 (i.e., where the polygon is either resident or breeding season)
 all_sp_layer.SetAttributeFilter("Seasonal = 1 or Seasonal = 2")
 
@@ -57,19 +30,22 @@ for i in range(all_sp_layer.GetFeatureCount()):
     sciname = feature.GetField('SCINAME')
     species.append(sciname)
 
-# unique list of site coordinates
-# loop over polygons for each point 
-# list of species names for each site
-
+#import sites
 inDataSource = ogr.Open("bbs_sites_coordinates_wrapper.vrt")
 site_lyr = inDataSource.GetLayer('bbs_sites')
 for feat in site_lyr:
     geom = feat.GetGeometryRef()
     print geom.ExportToWkt() 
-
-
+ 
+species_count = pd.DataFrame()
 for i in range(site_lyr.GetFeatureCount()):
+    num_species = []
     indv_site = site_lyr.GetFeature(i+1)
     indv_site_geom = indv_site.GetGeometryRef()
-    inter = first_species_geom.Intersection(indv_site_geom)
-    print inter
+    for i in range(all_sp_layer.GetFeatureCount()):
+        indv_species = all_sp_layer.GetFeature(i+1)
+        indv_species_geom = indv_species.GetGeometryRef()
+        inter = indv_site_geom.Intersection(indv_species_geom)
+        if inter != "GEOMETRYCOLLECTION EMPTY":
+            num_species.append(1)
+    species_count.append([indv_site.GetField('site'), len(num_species)])
