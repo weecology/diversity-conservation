9d6c9b56aaa417159a13cf40b6f3274b2e832cb8
Kari Norman <kari@normanfamily.org>
2015-07-14 13:43:58 -0600

    Return dataframe of centroid coordinates.

--- abundance_mapping.py
+++ abundance_mapping.py
@@ -63,6 +63,13 @@ def get_median_rarity_proportion(dataframe, species_column, proportion_column):
     med = np.median(uniq_prop)
     return med
 
+#find centroid of cell
+def get_centroid(points):  
+    x = [p[0] for p in points]
+    y = [p[1] for p in points]
+    centroid = (sum(x) / len(points), sum(y) / len(points))
+    return centroid
+
 
 #grid sampling
 def get_sites_by_grid(dataframe, site_col, lat_col, long_col, band_width, sites_in_cell):
@@ -75,6 +82,7 @@ def get_sites_by_grid(dataframe, site_col, lat_col, long_col, band_width, sites_
     lat_start = min_lat - 0.001
     lat_end = lat_start
     data_selection = pd.DataFrame()
+    centroid_coordinates = pd.DataFrame()
     while lat_end < max_lat:
         long_start = min_long - 0.001
         long_end = long_start
@@ -82,13 +90,21 @@ def get_sites_by_grid(dataframe, site_col, lat_col, long_col, band_width, sites_
         while long_end < max_long:
             long_end = long_end + band_degrees
             data_sub = dataframe[(dataframe[lat_col] > lat_start) & (dataframe[lat_col] < lat_end) & (dataframe[long_col] > long_start) & (dataframe[long_col] < long_end)]
+            corners = [(lat_start, long_start), (lat_start, long_end), (lat_end, long_start), (lat_end, long_end)]
             long_start = long_end
             if len(data_sub['site']) >= sites_in_cell:
+                centroid = get_centroid(corners)
                 selection = data_sub.ix[random.sample(data_sub.index, sites_in_cell)]
                 data_selection = data_selection.append(selection)
+                centroid_coordinates = centroid_coordinates.append([centroid, centroid, centroid])
+            else:
+                centroid = get_centroid(corners)
+                data_selection = data_selection.append([1, 1, 1, 1])
+                centroid_coordinates = centroid_coordinates.append([centroid])
         lat_start = lat_end
-    return data_selection
+    return data_selection, centroid_coordinates
 
+selected_sites, centroid_coordinates = get_sites_by_grid(data, 'site', 'lat', 'long', 100, 3)
 
 #SURVEY DATA
 data = pd.read_csv('bbs_abundances_by_site.csv', delimiter=',')
