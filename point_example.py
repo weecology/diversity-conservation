fdf6baad3696f2080badf4e0f5983f11d64c55b0
karinorman <kari@normanfamily.org>
2016-03-23 10:52:33 -0600

    Delete point_example.py

--- point_example.py
+++
@@ -1,7 +0,0 @@
-import ogr
-inDataSource = ogr.Open("bbs_sites_coordinates_wrapper.vrt")
-lyr = inDataSource.GetLayer("bbs_sites")
-for feat in lyr:
-    geom = feat.GetGeometryRef()
-    print geom.ExportToWkt()
-
